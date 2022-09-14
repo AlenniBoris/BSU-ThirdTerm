569678bc3bfa7190a702bf1ad453ea6477375e6e
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-09-14 10:35:38 +0300

    final lab2 OS

---
+++ EUGENE/HW/Vector/Vector/vectorH.h
@@ -0,0 +1,44 @@
+#ifdef MATHLIBRARY_EXPORTS
+#define MATHLIBRARY_API __declspec(dllexport)
+#else
+#define MATHLIBRARY_API __declspec(dllimport)
+#endif
+
+#include "numberH.h"
+#include <ostream>
+
+using namespace CalcsNum;
+class Vector {
+private:
+	Number _fNum, _sNum;
+public:
+	Vector(CalcsNum::Number fNum, Number sNum) : _fNum(fNum), _sNum(sNum){};
+
+	Vector operator+(const Vector& sVect) {
+		return Vector(_fNum + sVect._fNum, _sNum + sVect._sNum);
+	}
+
+	Number getRad() {
+		return Number(sqrt(((_fNum * _fNum) + (_sNum * _sNum)).getNum()));
+	}
+
+	Number getAngle() {
+		return Number(atan((_sNum / _fNum).getNum()));
+	}
+
+	Number getFNUM() {
+		return _fNum;
+	}
+
+	Number getSNUM() {
+		return _sNum;
+	}
+};
+
+const static Vector ZERO_V = Vector(ZERO, ZERO);
+const static Vector ONE_V = Vector(ONE, ONE);
+
+std::ostream& operator<<(std::ostream& out, Vector v) {
+	out << v.getFNUM() << ";" << v.getSNUM();
+	return out;
+}
