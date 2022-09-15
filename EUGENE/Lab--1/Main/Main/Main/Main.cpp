f25b8a80ac5a2f3fa744787bac84f93ba8575abc
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-09-15 11:33:10 +0300

    lab--1 refinished with  new foldering

---
+++ EUGENE/Lab--1/Main/Main/Main/Main.cpp
@@ -0,0 +1,10 @@
+﻿#include "Vector\Vector\Vector.h"
+
+int main()
+{
+    Vector fVec(Number(15), Number(40));
+    Vector sVec(Number(15), Number(-10));
+    std::cout << "Sum vecs = " << fVec + sVec << std::endl;
+    std::cout << ONE_V << std::endl;
+    std::cout << ZERO_V << std::endl;
+}
