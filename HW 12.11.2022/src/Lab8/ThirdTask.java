6f7c020e0168761db01dbfd227200c95e2236048
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-11-11 11:42:55 +0300

    full lab 8, lab 7

--- HW 12.11.2022/src/Lab8/ThirdTask.java
+++ HW 12.11.2022/src/Lab8/ThirdTask.java
@@ -7,5 +7,44 @@ import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 public class ThirdTask extends JFrame{
+    private ButtonGroup buttonGroup = new ButtonGroup();
+    private JRadioButton firstBtn = new JRadioButton();
+    private JRadioButton secondBtn = new JRadioButton();
 
+    public ThirdTask(){
+        setSize(1500,1500);
+        setVisible(true);
+        setLayout(new GridLayout(1,2));
+        add(firstBtn);
+        add(secondBtn);
+        firstBtn.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                firstBtn.setIcon(new ImageIcon("choosePicture.png"));
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                firstBtn.setIcon(new ImageIcon("pressPicture.png"));
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                firstBtn.setIcon(new ImageIcon("choosePicture.png"));
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                firstBtn.setIcon(new ImageIcon("onPicture.png"));
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+
+        buttonGroup.add(firstBtn);
+        buttonGroup.add(secondBtn);
+    }
 }
--- HW 12.11.2022/src/MainClass.java
+++ HW 12.11.2022/src/MainClass.java
@@ -1,11 +1,13 @@
 import Lab7.FrameClass;
 import Lab8.FirstTask;
 import Lab8.SecondTask;
+import Lab8.ThirdTask;
 
 public class MainClass {
     public static void main(String[] args) {
 //        Lab8.FirstTask lab8First = new FirstTask();
-        Lab8.SecondTask lab8Second = new SecondTask();
+//        Lab8.SecondTask lab8Second = new SecondTask();
+        Lab8.ThirdTask lab8Third = new ThirdTask();
 
 //        Lab7.FrameClass frameClass = new FrameClass();
     }
