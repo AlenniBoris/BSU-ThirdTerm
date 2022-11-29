372154a387b9894cabcb1cbf7cf092d6730ac081
Alennikov Boris <70971570+AlenniBoris@users.noreply.github.com>
2022-11-29 16:09:57 +0300

    ProcessM

---
+++ EUGENE/Lab--7/ProcessM/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    char enterNumbers[1024] = "1 2 3 4\n";
+    std::string outNumbers = "";
+
+    char *nextNumber = nullptr;
+    char *ptr = strtok_s(enterNumbers, " ", &nextNumber);
+
+    while(ptr != NULL){
+        outNumbers += std::to_string(std::atoi(ptr)*7) + " ";
+        ptr = strtok_s(NULL, " ", &nextNumber);
+    }
+
+    outNumbers += "\n";
+
+    std::cout << outNumbers;
+
+    return 0;
+}
